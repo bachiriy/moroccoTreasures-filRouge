d237a3d6005c356fcb9872c9d61f7f15d83cbe02
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-11 20:14:57 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ResetPWController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellerRequestController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\CheckIfUserAdmin;
+use App\Http\Middleware\CheckIfUserSeller;
+use App\Http\Middleware\validateProduct;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,16 +53,32 @@ Route::get('/shop', [ShopController::class, 'index']);
 Route::middleware('auth')->group(function () {
     // logout
     Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
-    // dashboard
-    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(CheckIfUserAdmin::class);
+    // Admin
+    Route::middleware(CheckIfUserAdmin::class)->group(function () {
+        // dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index']);
+        Route::get('/dashboard/categories', [DashboardController::class, 'categories']);
+        Route::get('/dashboard/users', [DashboardController::class, 'users']);
+        Route::get('/dashboard/products', [DashboardController::class, 'products']);
+        Route::get('/dashboard/requests', [DashboardController::class, 'requests']);
+        // seller requests
+        Route::post('/dashboard/requests/{user_id}/approve', [SellerRequestController::class, 'approve']);
+        Route::post('/dashboard/requests/{user_id}/deny', [SellerRequestController::class, 'deny']);
+    });
     // user
     Route::get('/profile', [UserController::class , 'index']);
     Route::put('/user/general-update', [UserController::class, 'gn_update']);
     Route::put('/user/password-update', [UserController::class, 'pw_update']);
 
     // products
-    Route::resource('products',ProductController::class);
-    Route::get('/products/create', [ProductController::class, 'create']);
+    Route::middleware(CheckIfUserSeller::class)->group(function(){
+        Route::resource('products',ProductController::class)->middleware(validateProduct::class);
+        Route::get('/products/create', [ProductController::class, 'create']);
+
+    });
+
+    // notifications
+    Route::delete('/notifications', [NotificationController::class, 'clear_all']);
 });
 
 Route::get('/about', function () {
