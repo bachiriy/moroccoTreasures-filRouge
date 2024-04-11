03838294a89e8976fa9833da18b0b27ad3fcac03
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-11 20:08:21 +0000

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,25 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('Pages.dashboard', ['page' => 'Dashboard']);
+        return view('Pages.dashboard.index', ['page' => 'Dashboard - Index']);
+    }
+
+    public function categories()
+    {
+        return view('Pages.dashboard.categories', ['page' => 'Dashboard - Categories']);
+    }
+
+    public function users()
+    {
+        return view('Pages.dashboard.users', ['page' => 'Dashboard - Users']);
+    }
+
+    public function products()
+    {
+        return view('Pages.dashboard.products', ['page' => 'Dashboard - Products']);
+    }
+    public function requests()
+    {
+        return view('Pages.dashboard.requests', ['page' => 'Dashboard - Requests']);
     }
 }
