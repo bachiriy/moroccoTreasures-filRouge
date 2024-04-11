2f6a9b08de477c6771a265ff1c1ea7b3a6bfe55d
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-11 20:08:45 +0000

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('Pages.shop', ['page' => 'Shop']);
+        $all_products = Product::all();
+        return view('Pages.shop', ['page' => 'Shop', 'all_products' => $all_products]);
     }
 }
