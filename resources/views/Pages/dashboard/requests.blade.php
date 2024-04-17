8bfcbd269304d32063130768cd92e82727406203
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-17 16:25:43 +0100

    update dash - requests view

--- resources/views/Pages/dashboard/requests.blade.php
+++ resources/views/Pages/dashboard/requests.blade.php
@@ -4,9 +4,11 @@
     <aside class="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
         <div>
             <div class="mt-8 text-center">
-                <img src="{{ asset('storage/' . Auth::user()->avatar) }}" alt="" class="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28">
-                <h5 class="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{{ Auth::user()->name }}</h5>
-                <span class="hidden text-gray-400 lg:block">{{ Auth::user()->role }}</span>
+                <a href="/profile">
+                    <img src="{{ asset('storage/' . Auth::user()->avatar) }}" alt="" class="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28">
+                    <h5 class="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{{ Auth::user()->name }}</h5>
+                    <span class="hidden text-gray-400 lg:block">{{ Auth::user()->role }}</span>
+                </a>
             </div>
 
             <ul class="space-y-2 tracking-wide mt-8">
@@ -79,7 +81,7 @@
             </button>
         </form>
     </aside>
-    <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+    <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] overflow-hidden">
         <div class="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
             <div class="px-6 flex items-center justify-between space-x-4 2xl:container">
                 <h5 hidden class="text-2xl text-gray-600 font-medium lg:block">Requests</h5>
@@ -88,6 +90,22 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
+                @if(session('success'))
+                    <p class="msg text-green-500 transition-all border border-green-500 px-4 py-1 rounded-sm">{{ session('success') }}</p>
+                @elseif(session('error'))
+                    <p class="msg text-red-500 transition-all border border-red-500 px-4 py-1 rounded-sm">{{ session('error') }}</p>
+                @endif
+                <script>
+                    let msgs = document.querySelectorAll('.msg')
+                    window.onload = function () {
+                        setInterval(() => {
+                            msgs.forEach((elm) => {
+                                elm.innerHTML = '';
+                                elm.style.border = 'none';
+                            });
+                        }, 3000);
+                    };
+                </script>
                 <div class="flex space-x-4">
                     <!--search bar -->
                     <button class="mx-4 middle none center flex items-center justify-center rounded-lg p-3 font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
@@ -96,7 +114,7 @@
                     </button>
                     <ul role="menu" data-popover="notifications-menu" data-popover-placement="bottom" style="z-index: 1000" class="absolute flex min-w-[180px] flex-col gap-2 overflow-auto rounded-md border border-blue-gray-50 bg-white p-3 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none">
                         @if(count(\App\Models\Notification::where('user_id', Auth::id())->get() ) > 0)
-                        @foreach(\Illuminate\Support\Facades\Auth::user()->notifications as $notification)
+                        @foreach(\Illuminate\Support\Facades\Auth::user()->notifications()->orderBy('created_at', 'desc')->get() as $notification)
                             <button role="menuitem" class="flex w-full cursor-pointer select-none items-center gap-4 rounded-md px-3 py-2 pr-8 pl-2 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
                                 <div class="flex flex-col gap-1">
                                     <p class="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased">
@@ -127,7 +145,72 @@
             </div>
         </div>
 
-        REQUESTS
+        <div class="flex flex-col">
+            <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+                <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full">
+                            <thead class="bg-white border-b">
+                            <tr>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    #
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Avatar
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Name
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Email
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Role
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+                                    Handle
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($requests as $request)
+                                <tr class="bg-gray-100 border-b hover:bg-gray-500/15">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        {{ $request->id }}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <img src="{{ asset('storage/' . $request->user->avatar ) }}" class="h-8 rounded-full w-8" alt="">
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{ $request->user->name }}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{ $request->user->email }}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{ $request->user->role }}
+                                    </td>
+                                    <td class="text-sm text-gray-900 flex justify-center font-light px-6 py-4 whitespace-nowrap">
+                                        <form action="/dashboard/requests/{{$request->user->id}}/approve" method="post" class="w-fit">
+                                            @csrf
+                                            <button class="text-green-500 mx-2 hover:underline" onclick="confirm('Are you sure you want to approve seller request ?')">Approve Seller</button>
+                                        </form>
+                                        <form action="/dashboard/requests/{{$request->user->id}}/deny" method="post" class="w-fit">
+                                            @csrf
+                                            <button class="text-red-500 mx-2 hover:underline" onclick="confirm('Are you sure you want to deny seller request ?')">Deny Seller</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    @if(count($requests) === 0)
+                        <p class="m-10">No seller requests were made.</p>
+                    @endif
+                </div>
+            </div>
+        </div>
     </div>
     <script type="module" src="https://unpkg.com/@material-tailwind/html@latest/scripts/popover.js"></script>
     <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
