8aeace32c562dd365f53231278b0f5a24cd4cb7f
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-17 16:16:42 +0100

    return each product rate to the shop view

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -8,7 +8,11 @@ class ShopController extends Controller
 {
     public function index()
     {
-        $all_products = Product::all();
+        $all_products = Product::with(['media', 'category'])->get();
+        foreach ($all_products as &$item) {
+            $item['rate'] = ReviewController::rate($item['id'])['rate'];
+        }
+//        dd($all_products);
         return view('Pages.shop', ['page' => 'Shop', 'all_products' => $all_products]);
     }
 }
