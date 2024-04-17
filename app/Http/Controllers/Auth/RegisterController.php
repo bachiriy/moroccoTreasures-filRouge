c0b0aadf7007a7388f760d11a820692eed36ac7c
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-17 16:11:33 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,6 @@ use App\Models\SellerRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
