b29c26f123bc825ac5e6faad4529ac9c125a9bfa
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-17 16:16:22 +0100

    update SellerRequestController returns

--- app/Http/Controllers/SellerRequestController.php
+++ app/Http/Controllers/SellerRequestController.php
@@ -6,18 +6,19 @@ use App\Models\Notification;
 use App\Models\SellerRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use function Webmozart\Assert\Tests\StaticAnalysis\false;
 
 class SellerRequestController extends Controller
 {
     public function deny(string $user_id)
     {
-        $return = SellerRequest::where('user_id', $user_id)->delete() ? '200' : 'error';
+        $rst = SellerRequest::where('user_id', $user_id)->delete() ?? false;
         Notification::create([
             'title' => 'Request to be a Seller.',
             'description' => 'We are sorry to inform you that your apply for being a seller on the website was denied.',
             'user_id' => $user_id,
         ]);
-        return $return;
+        return $rst ? back()->with('success', 'Seller request denied, this user will be just a buyer.') : back()->with('error', 'error denying seller request.');
     }
 
     public function approve(string $user_id)
@@ -31,6 +32,6 @@ class SellerRequestController extends Controller
             'description' => 'We are happy to inform you that your request to be a seller with us is approved successfully, enjoy making new things here.',
             'user_id' => $user_id,
         ]);
-        return '200';
+        return back()->with('success', 'Seller request Approved successfully, this user now is a Seller on the website.');
     }
 }
