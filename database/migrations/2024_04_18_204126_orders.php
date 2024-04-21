9c380e4b00261b0d2db6245687b47dfd1163086d
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-21 18:06:29 +0100

    add orders table migration

--- database/migrations/2024_04_18_204126_orders.php
+++ database/migrations/2024_04_18_204126_orders.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Product;
 
 return new class extends Migration
 {
@@ -11,7 +13,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('email');
+            $table->text('address');
+            $table->string('city');
+            $table->string('phone_number');
+            $table->foreignId('seller_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('orders');
     }
 };
