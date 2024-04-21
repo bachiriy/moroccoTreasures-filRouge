39639c65dbea54ec4b89de43308c23a030ac2b3b
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-21 18:05:49 +0100

    add Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'full_name',
+        'email',
+        'address',
+        'city',
+        'phone_number',
+        'seller_id',
+        'product_id'
+    ];
+
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
