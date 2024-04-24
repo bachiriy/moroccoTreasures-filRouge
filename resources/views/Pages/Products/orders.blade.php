6b1cebd69abe8c6ca21e95a696ccd3e67a048da6
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-24 23:29:49 +0100

    update seller orders page view

--- resources/views/Pages/Products/orders.blade.php
+++ resources/views/Pages/Products/orders.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('Layouts.navbar')
 
-    <div class="flex flex-col h-screen">
+    <div class="flex flex-col h-screen bg-gradient-to-r from-gray-900 to-red-950">
         @if(session('warn'))
             <p class="msg bg-white z-50 text-yellow-600 absolute m-5 p-2 border border-yellow-600">Warning : {{ session('warn') }} </p>
         @endif
@@ -19,13 +19,13 @@
             };
         </script>
 
-        <h1 class="text-xl text-center font-bold mt-10">Catalog of Your Ordered Products</h1>
-        <p class="text-gray-500 text-xs text-center mt-4 pb-6 border-b border-gray-200">Essential Step: Prioritize Direct Communication with Buyers to Confirm Orders, Ensuring Prompt Shipping and Customer Satisfaction.</p>
-        <div class=" sm:mx-0.5 lg:mx-0.5">
+        <h1 class="text-xl text-center font-bold mt-10 text-gray-300">Catalog of Your Ordered Products</h1>
+        <p class="text-gray-500 text-xs text-center mt-4 pb-6 border-b border-gray-300">Essential Step: Prioritize Direct Communication with Buyers to Confirm Orders, Ensuring Prompt Shipping and Customer Satisfaction.</p>
+        <div class="sm:mx-0.5 lg:mx-0.5">
             <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="">
                     <table class="min-w-full">
-                        <thead class="bg-white border-b">
+                        <thead class="bg-white border-b bg-gradient-to-r from-gray-700 to-red-600">
                         <tr>
                             <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                 #
@@ -55,19 +55,19 @@
                         </thead>
                         <tbody>
                         @foreach($orders as $order)
-                            <tr class="bg-gray-100 border-b hover:bg-gray-500/15">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            <tr class=" border-b bg-gray-500/15">
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-200">
                                     {{ $order->id }}
                                 </td>
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    <p class="relative group">
+                                    <p class="relative group text-gray-200">
                                         {{ $order->full_name }}
                                         <span class="absolute top-full left-0 w-64 rounded-lg bg-gray-800 text-white px-4 py-2 text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                             <span class="underline">email</span> :  {{ $order->email }}
                                         </span>
                                     </p>
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <td class="text-sm text-gray-200 font-light px-6 py-4 whitespace-nowrap">
                                     <p class="relative group">
                                         {{ $order->product->name }}
                                         <span class="absolute top-full left-0 w-64 rounded-lg bg-gray-800 text-white px-4 py-2 text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -82,13 +82,13 @@
                                         </span>
                                     </p>
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <td class="text-sm text-gray-200 font-light px-6 py-4 whitespace-nowrap">
                                     {{ $order->city }}
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <td class="text-sm text-gray-200 font-light px-6 py-4 whitespace-nowrap">
                                     {{ $order->address }}
                                 </td>
-                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <td class="text-sm text-gray-200 font-light px-6 py-4 whitespace-nowrap">
                                     {{ $order->phone_number }}
                                 </td>
                                 <td class="text-sm text-green-500 font-light px-6 py-4 whitespace-nowrap">
