3dc5ab8675d70476678c94db66c89306db081f2a
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-26 16:23:27 +0100

    add validate contact middleware

---
+++ app/Http/Middleware/ValidateContact.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateContact
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->validate([
+            'name' => 'required|string|min:5|max:250',
+            'email' => 'required|string|min:5|max:300|ends_with:gmail.com',
+            'phone' => 'required|integer',
+            'message' => 'required|min:5|string|max:1000',
+        ]);
+        return $next($request);
+    }
+}
