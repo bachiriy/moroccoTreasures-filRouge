6f7e2de79c0125c766de0cf29121ae3a969e6399
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-04-27 13:39:13 +0100

    update contact mail view

--- resources/views/Mails/contact.blade.php
+++ resources/views/Mails/contact.blade.php
@@ -1,6 +1,18 @@
-this is the contac: <br>
-
-{{ $infos->name }} <br>
-{{ $infos->email }} <br>
-{{ $infos->phone }} <br>
-{{ $infos->message }} <br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MHM New message</title>
+</head>
+<body>
+    <div>
+        <p>{{ $infos->message }}</p>
+    </div>
+    <h4>Sender Information</h4>
+    <p>Full Name : {{ $infos->name }}</p>
+    <p>Email : {{ $infos->email }}</p>
+    <p>Phone : {{ $infos->phone }}</p>
+</body>
+</html>
